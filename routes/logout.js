97efb3a418bcc0609b1e6fbe48d2afe6e687b2b1
J JITESH REDDY <79923685+jiteshjampa@users.noreply.github.com>
2024-08-03 09:57:31 +0530

    Update logout.js

--- routes/logout.js
+++ routes/logout.js
@@ -2,13 +2,26 @@
 const express = require("express");
 const router = express.Router();
 
-router.delete("/logout", (req, res) => {
-   res.clearCookie("authToken", {
+router.delete("/logout",  async (req, res) => {
+    try {
+      if (req.cookies.authToken) {
+        res.clearCookie("authToken", {
         httpOnly: true,
         secure: true, // Ensure this is true when serving over HTTPS
         sameSite: "None",
       });
-  res.status(200).json({ success: true, message: "Logged out successfully" });
-});
+        return res.status(200).send({
+          success: true,
+          message: "User Logged Out Successfully.",
+        });
+      }
+      return res.status(400).send({
+        success: false,
+        message: "User Not Logged In",
+      });
+    } catch (error) {
+      res.status(500).json({ success: false, error });
+    }
+  });
 
 module.exports = router;
