3031bfb8aca0729baf5cbf85b0963ba5d243ceba
J JITESH REDDY <79923685+jiteshjampa@users.noreply.github.com>
2026-06-15 14:31:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 db().then(() => {
   app.use(cookieParser());
-  app.use(cors({ origin: ["https://dine-delight.vercel.app","http://localhost:5173"], credentials: true }));
+ app.use(cors({
+  origin: ['https://dine-delight.vercel.app', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+app.options('*', cors()); 
   app.use(bodyparser.urlencoded({ extended: true }));
   app.use(bodyparser.json());
   app.get('/',(req,res)=>{
